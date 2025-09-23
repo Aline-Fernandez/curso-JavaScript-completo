a2374949a76ff2fed6f62914fce60bce3f9c05f4
Aline Fernandes <fernandes.dev@outlook.com>
2025-09-23 16:58:17 -0300

    tipo string

---
+++ revisao-logica/tipos-primitivos.js
@@ -0,0 +1,14 @@
+// string, number (int, floats), boolean
+
+let minhaVar = "minha string";
+let minhaVar2 = 'minha "string" ';
+var minhaVar3 = "minha tamplate literal"
+console.log(minhaVar3);
+
+
+let  idade = 31;
+// let msg = "eu possuo" + idade + "anos";
+let msg = `eu possuo ${idade} anos`;
+
+console.log(msg);
+console.log("Hello" + "world");
